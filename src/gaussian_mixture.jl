5c0b4b01ee58ad55ed0cebdb00355e668ad7f201
Alex Coventry <alx@empirical.com>
2016-06-25 16:49:01 -0400

    Move lpgamma function to where it is used

--- src/gaussian_mixture.jl
+++ src/gaussian_mixture.jl
@@ -57,6 +57,16 @@ function mean(nw::NormalWishart)
     return MvNormalCanon(T * nw.mu, T)
 end
 
+# Multidimensional gamma / partial gamma function
+# https://en.wikipedia.org/wiki/Wishart_distribution#Probability_density_function
+function lpgamma(p::Int, a::Float64)
+    res::Float64 = p * (p - 1.0) / 4.0 * log(pi)
+    for ii in 1:p
+        res += Distributions.lgamma(a + (1.0 - ii) / 2.0)
+    end
+    return res
+end
+
 function lognorm(nw::NormalWishart)
     return (nw.dim / 2) * (log(2 * pi) - log(nw.kappa)) + (nw.nu / 2) * logdet(nw.Tchol) + (nw.dim * nw.nu / 2) * log(2.) + lpgamma(nw.dim, nw.nu / 2)
 end
